ecdd42fe4ad5420dd93efef85df5d9d4b6aaf20b
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-06 11:58:43 +0200

    post manager done

---
+++ model/managers/PostManager.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Model\Managers;
+    
+    use App\Manager;
+    use App\DAO;
+    use Model\Managers\PostManager;
+
+    class PostManager extends Manager{
+
+        protected $className = "Model\Entities\Post";
+        protected $tableName = "post";
+
+
+        public function __construct(){
+            parent::connect();
+        }
+// Using a custom method because we need to find all the topics by ids 
+        public function findPostsInTopic($id)
+        {
+            $sql = "SELECT *
+                    FROM ".$this->tableName. " p
+                    WHERE p.topic_id = :id";
+                
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id'=>$id]),
+                $this->className
+            );
+        }
+
+    }
