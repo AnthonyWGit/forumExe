d0d898ef7ccb3da982b27de116470a80ed296d6f
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 09:22:56 +0200

    form for postip topics

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -18,14 +18,29 @@ if ($topics == null)
 }
 else
 {
-    foreach($topics as $topic ){
+    foreach($topics as $topic )
+    {
 
         ?>
         <p><a href="index.php?ctrl=post&action=postsList&id=<?=$topic->getId()?>"><?=$topic->getTitle()?></a></p>
         <?php
     }    
 }
+?>
 
+<h2 id="new_topic"> New topic Creation </h2>
 
+<div class="topicForm">
+<form id="topic_form" method="post" action="index.php?ctrl=topic&action=newTopic">
+
+    <label for="topicTitle">Topic tile </label>
+    <input type="text" id="topic_title"/>    
+
+    <label for="firstPost">First Post</label> 
+    <textarea id="topic_firstPost"></textarea>
+
+    <input type="submit" id="topic_validate" value="New Topic"/>
+</form>
+</div>
 
   
