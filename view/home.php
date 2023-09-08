8fd4e792ab99e1f2b1efa7d838f9803908549a85
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 20:29:34 +0200

    msg display

--- view/home.php
+++ view/home.php
@@ -1,9 +1,15 @@
+<?= isset($success) && $success == 1 ? "Register is complete" : "" ?>
+
 <h1>BIENVENUE SUR LE FORUM</h1>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+
+<?= isset($success) && $success == 1 ? "Register is complete" : "" ?>
 
 <p>
     <a href="/security/login.html">Se connecter</a>
     <span>&nbsp;-&nbsp;</span>
     <a href="index.php?ctrl=security&action=displayRegisterForm">S'inscrire</a>
 </p>
+
+<?php if (isset($_SESSION["sucess"])) unset($_SESSION["sucess"]) ?>
