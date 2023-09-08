9d731e421506b3128d88c550a445c05c8b8b8517
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 20:27:56 +0200

    template fior error msg list

--- view/security/error.php
+++ view/security/error.php
@@ -1,1 +1,14 @@
 <p>An error has occured</p>
+<?php
+foreach ($_SESSION["errors"] as $error) 
+{
+    ?>
+    
+    <ul>
+        <li><?= $error ?></li>
+    </ul>
+<?php
+}
+//cleaning session errors 
+unset($_SESSION["errors"]);
+?>
