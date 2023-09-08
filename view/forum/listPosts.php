6aa30d9bb977c7e62aef2076e74ef99dbfa58cba
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 08:44:34 +0200

    Comment where i use current 2 find topic title

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -1,9 +1,12 @@
 <?php
 
 $posts = $result["data"]["posts"]; //$posts contains generator objects. Preperties of those can only be accessed by doing iterations over them
-// var_dump($posts);
+$topic = $posts->current()->getTopic(); //current() allows us to access properties of the current objet. 
+//We use it before foreaching so we will always access properties of the first object
+//In this forum a topic will always have a post otherwise it doesn't exist so no problem
 ?>
 <?php
+echo $topic->getTitle();
 foreach($posts as $post)
 {
 ?>
@@ -17,3 +20,4 @@ foreach($posts as $post)
     </p>
     <?php
 }
+?>
