32d7bc12000304e97c526249df292e36e46b31ad
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 11:24:30 +0200

    adding namespace and manager and uses

---
+++ controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Controller;
+
+use App\Session;
+use App\AbstractController;
+use App\ControllerInterface;
+use Model\Managers\SecurityManager;
+
+class HomeController extends AbstractController implements ControllerInterface
+{
+
+    public function index()
+    {
+
+    }
+
+}
