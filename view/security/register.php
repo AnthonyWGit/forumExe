f598651ff7fda54f187bafd2e9c58de945a37368
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 16:07:56 +0200

    register form moved

---
+++ view/security/register.php
@@ -0,0 +1,13 @@
+<h2>Register form</h2>
+
+<form method="post" action="index.php?ctrl=security&action=validateForm" id="register_form" class="registerForm">
+    <label for="username">Username</label>
+    <input type="text" id="register_username" name="username"/>
+    <label for="email">Email</label>
+    <input type="email" id="register_email" name="email"/>
+    <label for="password">Password</label>
+    <input type="text" id="register_username" name="password"/> 
+    <label for="passwordConfirm">Password confirm</label>
+    <input type="text" id="register_username_validate" name="validatePassword"/>
+    <input type="submit" id="register_submit" value="Register"/>
+</form>
