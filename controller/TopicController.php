cd27f772cc49243c92f31442b6af8eb56c26f74d
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-08 11:24:55 +0200

    create newtopic with placeholder values

--- controller/TopicController.php
+++ controller/TopicController.php
@@ -6,12 +6,15 @@
     use App\AbstractController;
     use App\ControllerInterface;
     use Model\Managers\TopicManager;
+    use Model\Managers\PostManager;
     
-    class TopicController extends AbstractController implements ControllerInterface{
+    class TopicController extends AbstractController implements ControllerInterface
+    {
 
-        public function index(){
+        public function index()
+        {
+        }
           
-
         //    $topicManager = new TopicManager();
 
         //     return [
@@ -21,14 +24,32 @@
         //         ]
         //     ]; 
         //Block above shows all topics fromm all categories 
-        
-        $topicManager = new TopicManager();
+        public function listTopics()
+        {
 
-        return [
-            "view" => VIEW_DIR."forum/listTopics.php",
-            "data" => [
-                "topics" => $topicManager->findTopicsByCategory($_GET["id"])
-            ]
-            ];
+            $topicManager = new TopicManager();
+
+            return [
+                "view" => VIEW_DIR."forum/listTopics.php",
+                "data" => [
+                    "topics" => $topicManager->findTopicsByCategory($_GET["id"])
+                    ]
+                ];
+            
+
         }
+
+        public function newTopic()
+        {
+            $data = ["user_id" => 1 , "title" => $_POST["title"], "category_id" => $_GET["id"]];
+            $topicManager = new TopicManager();
+            $postManager = new PostManager();
+
+            $topicManager->createNewTopic($data);
+            $data=["user_id" => 1, "topic_id" => 1];
+            $postManager->createNewPost($data);
+
+            $this->redirectTo("topic","listTopics");            
+        }
+
     }
