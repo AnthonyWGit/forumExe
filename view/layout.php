509781951dda449d0a2f4e79d0ef17dea3de5d7d
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-12 15:05:38 +0200

    link to profile view

--- view/layout.php
+++ view/layout.php
@@ -34,7 +34,7 @@
                         
                         if(App\Session::getUser()){
                             ?>
-                            <a href="/security/viewProfile.html"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+                            <a href="index.php?ctrl=profile&action=viewProfile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
                             <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
                             <a href="index.php?ctrl=category&action=listCategories">la liste des catégories</a>
                             <?php
