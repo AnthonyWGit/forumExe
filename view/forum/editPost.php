4e5239bea134d379b580ae62a25f62c9804c606d
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-12 13:29:04 +0200

    view for post editing

---
+++ view/forum/editPost.php
@@ -0,0 +1,40 @@
+<?php
+
+$posts = $result["data"]["posts"]; //$posts contains generator objects. Preperties of those can only be accessed by doing iterations over them
+$topic = $posts->current()->getTopic(); //current() allows us to access properties of the current objet. 
+$content = $result["data"]["content"];
+//We use it before foreaching so we will always access properties of the first object
+//In this forum a topic will always have a post otherwise it doesn't exist so no problem
+
+?>
+<?php
+echo $topic->getTitle();
+foreach($posts as $post)
+{
+?>
+    <p>
+        <?= $post->getUser()->getUsername()?>
+        <?= (isset($_SESSION["user"]) && $_SESSION["user"] == "admin") ? '<a href="index.php?ctrl=post&action=deletePost&id='.$post->getId().'"> X  </a>' : ''?>
+        <?= (isset($_SESSION["user"]) && $_SESSION["user"] == $post->getUser()->getUsername()) ?  '<a href="index.php?ctrl=post&action=edit&id='.$post->getId().' "> Edit </a>' : '' ?>
+
+    </p>
+    <p>
+        <?= $post->getContent() ?>
+    </p>
+    <?php
+}
+?>
+<h2 id="new_post"> Post Edition</h2>
+
+<div class="postForm">
+<form id="post_form" method="post" action="index.php?ctrl=post&action=editConfirm&id=<?= $_GET["id"] ?>">
+
+    <label for="makePost">Post Content</label> 
+    <textarea id="makePost" name="makePost"><?=$content?></textarea>
+
+    <input type="submit" id="post_validate" value="New Post"/>
+</form>
+</div>
+
+  
+
