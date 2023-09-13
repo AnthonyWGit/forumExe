7569c54863a82d5dfa2b9e0d77c572439c32fc70
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-13 10:13:34 +0200

    display Userlist

---
+++ controller/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+    namespace Controller;
+
+    use App\Session;
+    use App\AbstractController;
+    use App\ControllerInterface;
+    use Model\Managers\TopicManager;
+    use Model\Managers\PostManager;
+    use Model\Managers\CategoryManager;
+    use Model\Managers\UserManager;
+    
+    class UserController extends AbstractController implements ControllerInterface
+    {
+
+        public function index()
+        {
+        }
+        
+        public function usersList()
+        {
+            $userManager = new UserManager();
+ 
+            return [
+                "view" => VIEW_DIR."forum/listUsers.php",
+                "data" => [
+                    "users" => $userManager->findAll(["username", "ASC"])
+                ]
+            ];
+        }
+    }
