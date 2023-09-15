889e8de61298cfcd4421e117d9fc12708411b34e
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-15 16:58:06 +0200

    change credentials template

--- view/security/redoCredentials.php
+++ view/security/redoCredentials.php
@@ -1,12 +1,12 @@
 <h2>Change credentials form</h2>
 
-<form method="post" action="index.php?ctrl=security&action=validateCredientials" id="register_form" class="registerForm">
+<form method="post" action="index.php?ctrl=security&action=validateCredentials" id="register_form" class="registerForm">
     <label for="username">Username</label>
     <input type="text" id="register_username" name="username"/>
     <label for="email">Email</label>
     <input type="email" id="register_email" name="email"/>
     <label for="password">Old Password</label>
-    <input type="text" id="old_register_password" name="oldpassword"/> 
+    <input type="text" id="old_register_password" name="oldPassword"/> 
     <label for="passwordConfirm">New Password</label>
     <input type="text" id="register_username_validate" name="validatePassword"/>
     <label for="passwordConfirm">New Password Confirm</label>
