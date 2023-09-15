cab31e0a0d8fd9a55210539ec941c0c75740f313
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-15 11:55:14 +0200

    added kick tools

--- view/forum/listUsers.php
+++ view/forum/listUsers.php
@@ -18,7 +18,19 @@ foreach($users as $user )
         <a href = "index.php?ctrl=admin&action=postsCreated&id=<?=$user->getId()?> ">See posts</a> &nbsp;
         <a href = "index.php?ctrl=admin&action=topicsCreated&id=<?=$user->getId()?> ">See topics</a>&nbsp;Mute&nbsp;
         <?= ($user->getState() == 'free') ? '<a href ="index.php?ctrl=admin&action=ban&id='.$user->getId().'">Ban</a>': '' ?>
-        <?= ($user->getState() == 'banned') ? '<a href ="index.php?ctrl=admin&action=unban&id='.$user->getId().'">Unban</a>': '' ?>
+        <?= ($user->getState() == 'banned' || $user->getState() == 'kicked') ? '<a href ="index.php?ctrl=admin&action=unban&id='.$user->getId().'">Unban</a>': '' ?>
+        <form method="post" action="index.php?ctrl=admin&action=timeout&id=<?= $user->getId() ?>">
+            <label for="timeout">Timeout : </label>
+            <input 
+            type="datetime-local"
+            id="calendar"
+            name="duration"
+            min="<?= date('Y-m-d\TH:i') ?>"
+            value="<?= date('Y-m-d\TH:i') ?>"
+            requierd />
+        <input type="submit" value="validate">
+        </form>
+
         <?= ($user->getRole() == 'mod' && $user->getRole() != 'admin') ? '<a href ="index.php?ctrl=admin&action=demote&id='.$user->getId().'">Demote to member</a>': '' ?>
         <?= ($user->getRole() != 'mod' && $user->getRole() != 'admin') ? '<a href ="index.php?ctrl=admin&action=promote&id='.$user->getId().'">Promote to moderator</a>': '' ?>
     </p>
