0f047dea8808cdf5498ebfb2ed034ab02f1638a5
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-17 10:28:04 +0200

    template to confirm delete account

---
+++ view/security/confirm.php
@@ -0,0 +1,7 @@
+<p>Are you sure you want to delete your account ? This action cannot be reversed</p>
+<form action="index.php?ctrl=security&action=confirmDelete" method="POST">
+<label for="password">Type your password here </label>
+<input type="password" name="password" id="password" required>
+<input type="submit" name="delete" value="Confirm">
+</form>
+
