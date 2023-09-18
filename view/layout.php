5e46855c224de644fb5df6ea626df28b00809f5e
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-18 09:13:36 +0200

    added meta desc

--- view/layout.php
+++ view/layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="<?= $metaDescription ?>">
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
@@ -35,8 +36,8 @@
                         if(App\Session::getUser()){
                             ?>
                             <a href="index.php?ctrl=profile&action=viewProfile" class="looksLikeAButton"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
-                            <a href="index.php?ctrl=security&action=logout" class="looksLikeAButton">Déconnexion</a>
-                            <a href="index.php?ctrl=category&action=listCategories" class="looksLikeAButton">la liste des catégories</a>
+                            <a href="index.php?ctrl=security&action=logout" class="looksLikeAButton">Disconnect</a>
+                            <a href="index.php?ctrl=category&action=listCategories" class="looksLikeAButton">Categories list</a>
                             <?php
                         }
                         // else{
@@ -55,7 +56,8 @@
             </main>
         </div>
         <footer>
-            <p>&copy; 2020 - Forum CDA - <a href="/home/forumRules.html" >Règlement du forum</a> - <a href="">Mentions légales</a></p>
+            <p>&copy; 2020 - Forum CDA - <a href="/home/forumRules.html" >Forum Rules</a>
+            <!-- <a href="">Mentions légales</a></p> -->
             <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
         </footer>
     </div>
