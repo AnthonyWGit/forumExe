99d185da1940b262c22251b3f8e3b1bdc1e21a73
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-18 09:13:13 +0200

    added meta desc

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -14,9 +14,10 @@
         public function index(){
             
             
-           
+
                 return [
-                    "view" => VIEW_DIR."home.php"
+                    "view" => VIEW_DIR."home.php",
+                    "metaDescription" => "Homepage"
                 ];
             }
             
